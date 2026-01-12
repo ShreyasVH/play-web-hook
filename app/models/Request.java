78c556e796b6fc20511fb5b4cf6252cd0daa7e77
ShreyasVH <shreyas.deve.1@gmail.com>
2026-01-13 00:39:28 +0530

    corrected name

--- app/models/Request.java
+++ app/models/Request.java
@@ -1,7 +1,6 @@
 package models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
